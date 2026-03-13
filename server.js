53ff97d4149ea6cf99d2d7cf44be8e2c74cbde30
DevopsRishu <devopsrishabh77@gmail.com>
2026-03-13 22:47:50 +0530

    Server.js bug fix

--- server.js
+++ server.js
@@ -6,28 +6,34 @@ import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
 import courseRoutes from "./routes/course.routes.js";
 import certificateRoutes from "./routes/certificate.routes.js";
-console.log("Server file loaded ✅");
 
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+// serve frontend files
+app.use(express.static("public"));
+
+// API routes
 app.use("/api/auth", authRoutes);
 app.use("/api/courses", courseRoutes);
 app.use("/api/certificates", certificateRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
-    console.log("MongoDB Connected ✅");
+
+  console.log("MongoDB Connected ✅");
+
   const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT} 🚀`);
-});
+  });
+
 })
 .catch((err) => {
-    console.error("MongoDB connection error ❌", err.message);
+  console.error("MongoDB connection error ❌", err.message);
 });
-app.use(express.static("public"));
